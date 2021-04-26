f9036ca3c7ec943c9f85bda799a575e02ce3d296
Kyle Simpson <getify@gmail.com>
2021-04-26 18:56:39 -0500

    updating dependencies (and fixing build script to match)

--- scripts/build-core.js
+++ scripts/build-core.js
@@ -4,7 +4,7 @@
 
 var fs = require("fs"),
 	path = require("path"),
-	ugly = require("terser"),
+	terser = require("terser"),
 	year = (new Date()).getFullYear(),
 
 	ROOT_DIR = path.join(__dirname,".."),
@@ -24,69 +24,71 @@ var fs = require("fs"),
 
 console.log("*** Building ESLint Plugin 'proper-arrows' ***");
 
-// read version number from package.json
-var packageJSON = JSON.parse(
-	fs.readFileSync(
-		path.join(ROOT_DIR,"package.json"),
-		{ encoding: "utf8", }
-	)
-);
-var version = packageJSON.version;
-
-// read copyright-header text, render with version and year
-var copyrightHeader = fs.readFileSync(
-	path.join(SRC_DIR,"copyright-header.txt"),
-	{ encoding: "utf8", }
-).replace(/`/g,"");
-copyrightHeader = Function("version","year",`return \`${copyrightHeader}\`;`)( version, year );
-
-
-// ***************************
-
-// try to make the dist directory, if needed
-try {
-	fs.mkdirSync(DIST_DIR,0o755);
-}
-catch (err) { }
-
-for (let [idx,SRC,] of LIB_SRC.entries()) {
-	let DIST = LIB_DIST[idx];
-
-	console.log(`Building: ${DIST}`);
-
+(async function main(){
+	// try to make the dist directory, if needed
 	try {
-		let result = "";
+		fs.mkdirSync(DIST_DIR,0o755);
+	}
+	catch (err) { }
 
-		result += fs.readFileSync(SRC,{ encoding: "utf8", });
+	// read version number from package.json
+	var packageJSON = JSON.parse(
+		fs.readFileSync(
+			path.join(ROOT_DIR,"package.json"),
+			{ encoding: "utf8", }
+		)
+	);
+	var version = packageJSON.version;
 
-		result = ugly.minify(result,{
-			mangle: {
-				keep_fnames: true,
-			},
-			compress: {
-				keep_fnames: true,
-			},
-			output: {
-				comments: /^!/,
-			},
-		});
+	// read copyright-header text, render with version and year
+	var copyrightHeader = fs.readFileSync(
+		path.join(SRC_DIR,"copyright-header.txt"),
+		{ encoding: "utf8", }
+	).replace(/`/g,"");
+	copyrightHeader = Function("version","year",`return \`${copyrightHeader}\`;`)( version, year );
 
-		// was compression successful?
-		if (!(result && result.code)) {
-			if (result.error) throw result.error;
-			else throw result;
+
+	// ***************************
+
+	for (let [idx,SRC,] of LIB_SRC.entries()) {
+		let DIST = LIB_DIST[idx];
+
+		console.log(`Building: ${DIST}`);
+
+		try {
+			let result = "";
+
+			result += fs.readFileSync(SRC,{ encoding: "utf8", });
+
+			result = await terser.minify(result,{
+				mangle: {
+					keep_fnames: true,
+				},
+				compress: {
+					keep_fnames: true,
+				},
+				output: {
+					comments: /^!/,
+				},
+			});
+
+			// was compression successful?
+			if (!(result && result.code)) {
+				if (result.error) throw result.error;
+				else throw result;
+			}
+
+			// append copyright-header text
+			result = `${copyrightHeader}${result.code}`;
+
+			// write dist
+			fs.writeFileSync( DIST, result, { encoding: "utf8", } );
 		}
-
-		// append copyright-header text
-		result = `${copyrightHeader}${result.code}`;
-
-		// write dist
-		fs.writeFileSync( DIST, result, { encoding: "utf8", } );
+		catch (err) {
+			console.error(err);
+			process.exit(1);
+		}
 	}
-	catch (err) {
-		console.error(err);
-		process.exit(1);
-	}
-}
 
-console.log("Complete.");
+	console.log("Complete.");
+})();
